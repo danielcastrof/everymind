4a1d48197c59faaf27d5b501f94a0d8c85795453
Daniel Castro de Farias <45545000+danielcastrof@users.noreply.github.com>
2023-12-27 16:05:16 -0300

    feat: adicionando swagger

--- nunes-sports-backend/src/main.ts
+++ nunes-sports-backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - Desafio Everymind')
+    .setDescription(
+      'Projeto back-end.',
+    )
+    .setVersion('1.0')
+    .addTag('Produtos')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
