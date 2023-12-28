08929454f2e54ffee63c01f64a51f99318a0885e
Daniel Castro de Farias <45545000+danielcastrof@users.noreply.github.com>
2023-12-28 13:33:02 -0300

    fix: ajustes integração

--- nunes-sports-frontend/src/services/api/busca-por-nome/index.ts
+++ nunes-sports-frontend/src/services/api/busca-por-nome/index.ts
@@ -1,6 +1,6 @@
 import {api} from '../index';
 
 export const buscaId = async (id: string) => {
-    const url = '/produto/' + id;
+    const url = '/produto/nome/' + id;
      return api.get(url);
   }
